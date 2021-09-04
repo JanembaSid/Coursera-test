dd408538e688007b7d018e76833cf76999cd6544
JanembaSid <88332207+JanembaSid@users.noreply.github.com>
2021-09-04 09:48:14 +0530

    Update SpeakGoodbye.js

--- Module-4/SpeakGoodbye.js
+++ Module-4/SpeakGoodbye.js
@@ -22,11 +22,10 @@
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
 (function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
 })(window);
