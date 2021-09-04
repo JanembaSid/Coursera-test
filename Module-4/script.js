e0c47ef6cc0aab31112e5313b20040197a4d78ba
JanembaSid <88332207+JanembaSid@users.noreply.github.com>
2021-09-04 09:55:48 +0530

    Update script.js

--- Module-4/script.js
+++ Module-4/script.js
@@ -1,3 +1,4 @@
+(function (window) {
 // *******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
@@ -32,13 +33,14 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
 // See Lecture 50, part 1
-
+for (var i in names) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -47,27 +49,17 @@ WARNING!!! WARNING!!!
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  
-
-(function(){
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
-  for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-  
-    if (firstLetter === 'j') {
-      byeSpeaker(names[i]);
-    }
-    else {
-      helloSpeaker(names[i]);
-    }
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
   }
-
-
-  })();
+}
+})(window);
